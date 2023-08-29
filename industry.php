77f5c3699d6a5f631f52245d47d6562ae9182902
Clement Samuel <52810618+TheSamclem@users.noreply.github.com>
2023-08-29 15:25:00 +0100

    first

---
+++ industry.php
@@ -0,0 +1,427 @@
+<?php 
+    require_once('header.php');
+?>
+       
+    <!-- landingpage Hero Section -->
+    <section class="header position-relative">
+
+        <div class="image-container">
+            <img src="/img/industry-head.jpg" alt="" class="img-fluid">
+        </div>
+
+       <div class="container h-100 px-lg-5 ms-sm-5 position-absolute top-0">
+        <div class="row">
+            <div class="enterprise h-100 d-flex align-items-center p-4">
+
+                <div>
+
+                    <h1 class="pb-sm-4" data-aos="fade-right">
+                        Industries
+                    </h1>
+
+        
+                </div>
+            </div>
+        </div>
+
+       </div>
+
+    </section>
+    <!--  -->
+
+    <!-- Indusries List -->
+    <section class="our-srv p-4" style="background-color: black;">
+        <div class="container py-5">
+
+            <!-- Industry Card -->
+            <div class="row d-flex justify-content-around g-4 mt-4">
+
+                <!--Service One  -->
+                <div class="col-lg-12 serv-btn justify-content-around">
+
+                    <div class="row d-flex justify-content-around g-4">
+                        
+                        <!-- Primary Sector Card -->
+                        <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                            <div class="card-body p-0">
+                                <img src="/Industries-img/primary-sector-concept-icon-product-fabrication-vector-29170261.png" alt="" class="img-fluid">
+                              
+                            </div>
+
+                            <p  class="py-5 d-flex justify-content-center" style="text-align: center !important;">
+                                Government & Private Sector
+                            </p>
+
+                        </div>
+
+        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/airplane.png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="py-5 d-flex justify-content-center">
+                                    Aviation
+    
+                                </p>
+    
+                            </div>
+
+
+                        
+                        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/telecommunication.png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="py-5 d-flex justify-content-center">
+                                    Telecommunication
+
+    
+                                </p>
+    
+                            </div>
+
+
+                        
+                        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/police-officer-3-512.png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="  py-5 d-flex justify-content-center">
+                                    Law enforcement agencies
+                                </p>
+    
+                            </div>
+
+                    </div>
+
+                </div>
+        
+                
+              
+            </div>
+
+            <!-- Industry Card -->
+            <div class="row d-flex justify-content-around g-4 mt-4">
+
+                    <!--Service One  -->
+                    <div class="col-lg-12 serv-btn justify-content-around">
+    
+                        <div class="row d-flex justify-content-around g-4">
+                            
+                            <!-- Primary Sector Card -->
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+    
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/-oil- and gas .png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="  py-5 d-flex justify-content-center">
+                                    Mining Oil & Gas
+
+    
+                                </p>
+    
+                            </div>
+    
+            
+                                <div class="card col-lg-3 p-0" style="width: 18rem;">
+    
+                                    <div class="card-body p-0">
+                                        <img src="/Industries-img/land-proffessionals .png" alt="" class="img-fluid">
+                                      
+                                    </div>
+        
+                                    <p  class="py-5 d-flex justify-content-center">
+                                        Land Professionals
+
+        
+                                    </p>
+        
+                                </div>
+    
+    
+                            
+                            
+                                <div class="card col-lg-3 p-0" style="width: 18rem;">
+    
+                                    <div class="card-body p-0">
+                                        <img src="/Industries-img/Manufacturing .png" alt="" class="img-fluid">
+                                      
+                                    </div>
+        
+                                    <p  class="py-5 d-flex justify-content-center">
+                                        Manufacturing 
+
+    
+        
+                                    </p>
+        
+                                </div>
+    
+    
+                            
+                            
+                                <div class="card col-lg-3 p-0" style="width: 18rem;">
+    
+                                    <div class="card-body p-0">
+                                        <img src="/Industries-img/utilities.png" alt="" class="img-fluid">
+                                      
+                                    </div>
+        
+                                    <p  class="  py-5 d-flex justify-content-center">
+                                        Utilities
+
+                                    </p>
+        
+                                </div>
+    
+                        </div>
+    
+                    </div>
+            
+                    
+                  
+            </div>
+
+            <!-- Industry Card -->
+            <div class="row d-flex justify-content-around g-4 mt-4">
+
+                <!--Service One  -->
+                <div class="col-lg-12 serv-btn justify-content-around">
+
+                    <div class="row d-flex justify-content-around g-4">
+                        
+                        <!-- Primary Sector Card -->
+                        <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                            <div class="card-body p-0">
+                                <img src="/Industries-img/NON profit organization.png" alt="" class="img-fluid">
+                              
+                            </div>
+
+                            <p  class="  py-5 d-flex justify-content-center">
+                                Non-Profit Organisation
+
+
+
+                            </p>
+
+                        </div>
+
+        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/Health care and .png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="py-5 d-flex justify-content-center" style="text-align: center !important;">
+                                    Healthcare and Community Services
+    
+                                </p>
+    
+                            </div>
+
+
+                        
+                        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/Transport and logistics .png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="py-5 d-flex justify-content-center">
+                                    Transport and Logistics
+
+                                </p>
+    
+                            </div>
+
+
+                        
+                        
+                            <div class="card col-lg-3 p-0" style="width: 18rem;">
+
+                                <div class="card-body p-0">
+                                    <img src="/Industries-img/educational-institution-educational-institutions-icon-png.png" alt="" class="img-fluid">
+                                  
+                                </div>
+    
+                                <p  class="  py-5 d-flex justify-content-center">
+                                    Education
+
+                                </p>
+    
+                            </div>
+
+                          
+
+                           
+                        </div>
+                    </div>
+
+            </div>
+           
+
+            </div>
+                    
+        </div>
+
+
+        </div>
+
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer border-top">
+
+        <div class="container py-sm=5">
+
+
+            <div class="row">
+
+                <div class="col-lg-3 p-0">
+                    <a href="#"><img src="/img/DIAL SAFE LOGO-02.png" alt="" class="img-fluid" style="height: 120px; width: 120px;"></a>
+                </div>
+
+                
+                <div class="col-md-3 mt-4">
+                    <h5 class="pt-3 fw-bold" style="color: rgb(210, 210, 210);">Company</h5>
+                    <a href="#">About</a> <br>
+                    <a href="#">Careers at Dial 24/7</a> <br>
+                    <a href="#">Contact Us</a> <br>
+                    <a href="#">Privacy & Security</a> <br>
+                </div>
+             
+                <div class="col-md-3 mt-4">
+                    <h5 class="pt-3 fw-bold" style="color:  rgb(210, 210, 210);">Product</h5>
+                    <a href="#">Pricing</a> <br>
+                    <a href="#">Integrations</a> <br>
+                    <a href="#">Get Started For Free</a> <br>
+                    <a href="#">Request a Demo</a> <br>
+                </div>
+             
+                <div class="col-md-3 mt-4">
+                    <h5 class="pt-3 fw-bold" style="color:  rgb(210, 210, 210);">Resources</h5>
+                    <a href="#">Case Studies</a> <br>
+                    <a href="#">HSE</a> <br>
+                    <a href="#">Blog</a> <br>
+                    <a href="#">Legislation</a> <br>
+                </div>
+             
+
+            </div>
+
+               
+            <div class="row">
+
+                <div class="col-lg-12 d-flex justify-content-between text-white add py-sm-5">
+                   
+                    <div class="col-lg-6 add">
+                        Email  <a href="" class="text-warning">
+                            info@dial24-7portal.com
+                        </a>
+    
+                    </div>
+
+                    <p>
+                        plot 12, House 6, 2nd Avenue, Federal Housing Estate, Trans Amadi Ind. Layout, PH
+                    </p>
+
+                </div>
+
+            </div> 
+
+        </div>
+
+    </footer>
+
+    
+    <section class="socials border-top p-4" style="background-color: black;">
+        <div class="container">
+            <div class="row ">
+
+                <div class="col-lg-12 p-0 d-flex justify-content-center">
+                    
+                    <div>
+                        <a href=""><i class="ri-linkedin-box-fill fs-3 text-white" id="linkedin"></i></a>
+                        <a href=""><i class="ri-instagram-fill fs-3 text-white ms-3" id="instagram"></i></a>
+                        <a href=""><i class="ri-twitter-x-line fs-3 text-white ms-3" id="twitter"></i></a>
+                    </div>
+                   
+                </div>
+               
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
+
+<!-- Bootsrap Js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+<!-- Main JS script -->
+<script src="main.js"></script>
+<!-- AOS script -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init({
+    duration: 1000,
+    once: true,
+  });
+</script>
+
+<script src="path-to-the-file/splide.min.js"></script>
+</body>
+</html>
